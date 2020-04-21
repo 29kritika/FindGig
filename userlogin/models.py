05a9df9cfe1b7c86952f230538dc5eff95773667
shreeparnadey <44523378+shreeparnadey@users.noreply.github.com>
2020-04-21 12:59:37 +0530

    'added_djongo'

--- userlogin/models.py
+++ userlogin/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, null=False, db_index=True)
+    type = models.TextField(max_length=500, blank=True)
+
+
+# class Song(models.Model):
+#     id = models.TextField(max_length=10, unique=True, null=False, db_index=True)
+#     name = models.TextField(max_length=200, null=False)
+#     Genre = models.TextField(max_length=200, null=False)
+#
+#
+# class Relation(models.Model):
+#     singer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#     song_sang = models.ForeignKey(Song, on_delete=models.CASCADE)
